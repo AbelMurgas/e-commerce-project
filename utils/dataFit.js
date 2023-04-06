25cfdceaa747b5616e697310948dccf98a961c1d
Abel Murgas <abelmurgasdev@gmail.com>
2023-04-06 00:17:33 -0500

    deleted unnecessary process

--- utils/dataFit.js
+++
@@ -1,5 +0,0 @@
-export const objectErrorToString = (errorsData) => {
-  const messageList = errorsData.errors.map((obj) => obj.msg);
-  const messagesString = messageList.join("\n");
-  return messagesString;
-};
