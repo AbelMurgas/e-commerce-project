aa5eb3711a9098e7a955bdc8e340a056619d9292
Abel Murgas <abelmurgasdev@gmail.com>
2023-04-06 00:18:03 -0500

    create file management utils

---
+++ utils/file.js
@@ -0,0 +1,23 @@
+import path from "path";
+import fs from "fs"
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = path.dirname(__filename);
+
+export default class file {
+ static clearImage = (filePath) =>{
+  if (!filePath){
+   console.log("no file received")
+   return false
+  }
+  filePath = path.join(__dirname, "..", filePath);
+  if (fs.existsSync(filePath)) {
+   fs.unlink(filePath, err => {
+    console.log(err)
+   })
+   return true 
+  }
+ }
+}
