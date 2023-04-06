d700f83c1d7158b11a3c356b1f4939d94728bdd4
Abel Murgas <abelmurgasdev@gmail.com>
2023-04-06 00:18:49 -0500

    add new route

--- routes/product.js
+++ routes/product.js
@@ -1,11 +1,15 @@
 import express from "express";
-import { getProducts, getProduct } from "../controllers/product.js";
+import { getProducts, getProduct, postProduct } from "../controllers/product.js";
 import { productInputValidator } from "../utils/validator.js";
 
 const router = express.Router();
 
+// /product
+
 router.get("/", getProducts);
 
 router.get("/:productId", productInputValidator, getProduct);
+
+router.post("/", postProduct);
 
 export default router;
