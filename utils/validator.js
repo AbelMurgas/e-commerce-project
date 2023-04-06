9f762f0b10e5cf78d1788316711856ec6dce0405
Abel Murgas <abelmurgasdev@gmail.com>
2023-04-06 01:19:48 -0500

    created post product validator

--- utils/validator.js
+++ utils/validator.js
@@ -1,14 +1,44 @@
 import validator from "express-validator";
 const { body, param } = validator;
-import { Types } from 'mongoose';
+import { Types } from "mongoose";
 const ObjectId = Types.ObjectId;
 
-export const productInputValidator = [
-  param("productId")
-    .custom((value, { req }) => {
-      if (!ObjectId.isValid(value)) {
-        throw new Error("Invalid product ID");
-      }
-      return true;
-    })
+export const getProductValidator = [
+  param("productId").custom((value, { req }) => {
+    if (!ObjectId.isValid(value)) {
+      throw new Error("Invalid product ID");
+    }
+    return true;
+  }),
+];
+
+export const postProductValidator = [
+  body("title")
+    .exists().withMessage("Title is required")
+    .isLength({ min: 5, max: 50 })
+    .withMessage("Title must have a length between 5 and 50 characters"),
+  body("description")
+    .exists()
+    .withMessage("Description is required")
+    .isLength({ min: 5, max: 255 })
+    .withMessage("Description must have a length between 5 and 255 characters"),
+  body("category").exists().withMessage("Category is required"),
+  body("price")
+    .exists()
+    .withMessage("Price is required")
+    .isNumeric()
+    .withMessage("Price must be numeric")
+    .isFloat({ min: 0, max: 10000 })
+    .withMessage("Price must be between 0 and 10000"),
+  body("discount")
+    .optional()
+    .isFloat({ min: 0, max: 1 })
+    .withMessage("Price must be between 0 and 1"),
+  body("amountInStock").exists().withMessage("Amount In Stock is required"),
+  body("image").custom((value, { req }) => {
+    if (!req.file) {
+      throw new Error("Image not receive");
+    }
+    return true;
+  }),
 ];
