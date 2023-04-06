23b4b700799656e103e7d7998812ec720adea7de
Abel Murgas <abelmurgasdev@gmail.com>
2023-04-06 00:18:37 -0500

    created postProduct controller

--- controllers/product.js
+++ controllers/product.js
@@ -1,5 +1,5 @@
 import Product from "../models/product.js";
-import { objectErrorToString } from "../utils/dataFit.js";
+import file from "../utils/file.js"
 
 import validator from "express-validator";
 
@@ -26,4 +26,34 @@ export const getProduct = async (req, res, next) => {
   } catch (err) {
     next(err);
   }
+};
+
+export const postProduct = async (req, res, next) => {
+  const title = req.body.title;
+  const description = req.body.description;
+  const category = req.body.category;
+  const price = req.body.price;
+  const discount = req.body.discount;
+  const amountInStock = req.body.amountInStock;
+  const normalizedPath = req.file.path.replace(/\\/g, '/');
+  const errorsObject = validationResult(req);
+  try {
+    if (!errorsObject.isEmpty()) {
+      return res.status(400).json({ errors: errorsObject.array() });
+    }
+    const product = new Product({
+      title: title,
+      description: description,
+      imageUrl: normalizedPath,
+      category: category,
+      price: price,
+      discount: discount,
+      amountInStock: amountInStock
+    })
+    const productCreated = await product.save()
+    return res.status(201).json({ product: productCreated });
+  } catch (err) {
+    next(err);
+  }
+  
 };
