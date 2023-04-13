838f85adc37a6546a5090956920b717ce0070b6b
AbelMurgas <abelmurgasdev@gmail.com>
2023-04-13 04:31:46 -0500

    create validator for updated product

--- utils/file.js
+++ utils/file.js
@@ -1,5 +1,5 @@
 import path from "path";
-import fs from "fs"
+import fs from "fs";
 import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
@@ -7,17 +7,17 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default class file {
- static clearImage = (filePath) =>{
-  if (!filePath){
-   console.log("no file received")
-   return false
-  }
-  filePath = path.join(__dirname, "..", filePath);
-  if (fs.existsSync(filePath)) {
-   fs.unlink(filePath, err => {
-    console.log(err)
-   })
-   return true 
-  }
- }
+  static clearImage = (filePath) => {
+    if (!filePath) {
+      console.log("no file received");
+      return false;
+    }
+    filePath = path.join(__dirname, "..", filePath);
+    if (fs.existsSync(filePath)) {
+      fs.unlink(filePath, (err) => {
+        console.log(err);
+      });
+      return true;
+    }
+  };
 }
--- utils/validator.js
+++ utils/validator.js
@@ -1,6 +1,7 @@
 import validator from "express-validator";
 const { body, param } = validator;
 import { Types } from "mongoose";
+import Product from "../models/product.js";
 const ObjectId = Types.ObjectId;
 
 export const getProductValidator = [
@@ -35,6 +36,44 @@ export const postProductValidator = [
     .isFloat({ min: 0, max: 1 })
     .withMessage("Price must be between 0 and 1"),
   body("amountInStock").exists().withMessage("Amount In Stock is required"),
+  body("image").custom((value, { req }) => {
+    if (!req.file) {
+      throw new Error("Image not receive");
+    }
+    return true;
+  }),
+];
+
+// TODO: Validate if product id exist in update product
+export const putProductValidator = [
+  param("productId").custom( async (value, { req }) => {
+    if (!ObjectId.isValid(value)) {
+      throw new Error("Invalid product ID");
+    }
+    const productMatchId = await Product.findById(value)
+    if (!productMatchId) {
+      throw new Error("Product not exist");
+    }
+    return true;
+  }),
+  body("title")
+    .optional()
+    .isLength({ min: 5, max: 50 })
+    .withMessage("Title must have a length between 5 and 50 characters"),
+  body("description")
+    .optional()
+    .isLength({ min: 5, max: 255 })
+    .withMessage("Description must have a length between 5 and 255 characters"),
+  body("price")
+    .optional()
+    .isNumeric()
+    .withMessage("Price must be numeric")
+    .isFloat({ min: 0, max: 10000 })
+    .withMessage("Price must be between 0 and 10000"),
+  body("discount")
+    .optional()
+    .isFloat({ min: 0, max: 1 })
+    .withMessage("Price must be between 0 and 1"),
   body("image").custom((value, { req }) => {
     if (!req.file) {
       throw new Error("Image not receive");
