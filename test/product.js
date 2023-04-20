29377f5ec6994433354714c856f0f50926c0c47b
AbelMurgas <abelmurgasdev@gmail.com>
2023-04-20 11:53:20 -0500

    create POST product test

--- test/product.js
+++ test/product.js
@@ -15,12 +15,34 @@ describe("GET /product/:productId", function () {
   it("It should throw an error if it allow an id not supported by mongoDB", function (done) {
     request(app)
       .get("/products/12ds")
-      .expect(400)
+      .expect(422)
       .end(function (err, res) {
         if (err) return done(err);
         expect(res.body.errors).to.be.an("array");
-        expect(res.body.errors).to.have.lengthOf(1);
         expect(res.body.errors[0].msg).to.equal("Invalid product ID");
+        done();
+      });
+  });
+});
+
+describe("Post /product/", function () {
+  before(async () => {
+    await mongoose.connect(config.MONGO_URI,
+      { useNewUrlParser: true });
+  });
+  after(async () => {
+    await mongoose.connection.close();
+  });
+
+  it("It should throw an error if not receive any data of the new product", function (done) {
+    const requestBody = null
+    request(app)
+      .post("/products/")
+      .send(requestBody)
+      .expect(422)
+      .end(function (err, res) {
+        if (err) return done(err);
+        expect(res.body.errors).to.be.an("array");
         done();
       });
   });
