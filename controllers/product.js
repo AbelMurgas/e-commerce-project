e8ee26772f8ae884aae3c3d91159668623e5c661
AbelMurgas <abelmurgasdev@gmail.com>
2023-04-20 11:54:07 -0500

    refactoring and fix some bugs

--- controllers/product.js
+++ controllers/product.js
@@ -5,6 +5,10 @@ import validator from "express-validator";
 
 const { validationResult } = validator;
 
+const HTTP_STATUS_CREATED = 201;
+const HTTP_STATUS_OK = 200;
+const HTTP_STATUS_UNPROCESSABLE_ENTITY = 422;
+
 export const getProducts = async (req, res, next) => {
   try {
     const products = await Product.find();
@@ -19,88 +23,77 @@ export const getProduct = async (req, res, next) => {
   const errorsObject = validationResult(req);
   try {
     if (!errorsObject.isEmpty()) {
-      return res.status(400).json({ errors: errorsObject.array() });
+      return res.status(HTTP_STATUS_UNPROCESSABLE_ENTITY).json({ errors: errorsObject.array() });
     }
     const product = await Product.findById(productId);
-    res.status(201).json({ data: product });
+    res.status(200).json({ data: product });
   } catch (err) {
     next(err);
   }
 };
 
 export const postProduct = async (req, res, next) => {
-  const title = req.body.title;
-  const description = req.body.description;
-  const category = req.body.category;
-  const price = req.body.price;
-  const discount = req.body.discount;
-  const amountInStock = req.body.amountInStock;
+  const { title, description, category, price, discount, amountInStock } = req.body;
   const errorsObject = validationResult(req);
+  const imageUrl = req.file?.path?.replace(/\\/g, "/");
   try {
     if (!errorsObject.isEmpty()) {
-      file.clearImage(req.file.path);
-      return res.status(400).json({ errors: errorsObject.array() });
+      return res.status(HTTP_STATUS_UNPROCESSABLE_ENTITY).json({ errors: errorsObject.array() });
     }
-    const normalizedPath = req.file.path.replace(/\\/g, "/");
     const product = new Product({
-      title: title,
-      description: description,
-      imageUrl: normalizedPath,
-      category: category,
-      price: price,
-      discount: discount,
-      amountInStock: amountInStock,
+      title,
+      description,
+      imageUrl,
+      category,
+      price,
+      discount,
+      amountInStock,
     });
     const productCreated = await product.save();
     return res.status(201).json({ product: productCreated });
   } catch (err) {
-    file.clearImage(req.file.path);
     next(err);
+  } finally {
+    if (res.statusCode !== 201) {
+      file.clearImage(imageUrl)
+    }
   }
 };
 
 export const updateProduct = async (req, res, next) => {
-  const productId = req.params.productId;
-  const title = req.body.title;
-  const description = req.body.description;
-  const category = req.body.category;
-  const price = req.body.price;
-  const discount = req.body.discount;
-  const amountInStock = req.body.amountInStock;
-  const wasFilePassed = req.file ? true : false;
-  const normalizedPath = (wasFilePassed) ? req.file.path.replace(/\\/g, "/") : undefined;
+  const { title, description, category, price, discount, amountInStock } = req.body;
   const errorsObject = validationResult(req);
+  const imageUrl = req.file?.path?.replace(/\\/g, "/");
+  const productId = req.params.productId;
+  let productBeforeUpdated = null
   try {
     if (!errorsObject.isEmpty()) {
-      if (normalizedPath){
-        file.clearImage(normalizedPath)
-      };
-      return res.status(400).json({ errors: errorsObject.array() });
+      return res.status(HTTP_STATUS_UNPROCESSABLE_ENTITY).json({ errors: errorsObject.array() });
     }
     const updateFields = {
-      title: title,
-      description: description,
-      imageUrl: normalizedPath,
-      category: category,
-      price: price,
-      discount: discount,
-      amountInStock: amountInStock,
+      title,
+      description,
+      imageUrl,
+      category,
+      price,
+      discount,
+      amountInStock,
     };
-    const productImagePath = await Product.findById(productId).select("imageUrl").imageUrl
-    console.log(normalizedPath)
+    productBeforeUpdated = await Product.findById(productId).select("imageUrl")
     const productUpdated = await Product.findByIdAndUpdate(
       { _id: productId },
       { $set: updateFields },
       { upsert: false, new: true }
     );
-    if (normalizedPath){
-      file.clearImage(productImagePath)
-    };
-    res.status(201).json({ productUpdated: productUpdated });
+    res.status(HTTP_STATUS_CREATED).json({ productUpdated: productUpdated });
   } catch (err) {
-    if (normalizedPath){
-      file.clearImage(normalizedPath)
-    }
     next(err);
+  } finally {
+    console.log(res.statusCode, imageUrl)
+    if (res.statusCode === HTTP_STATUS_CREATED && imageUrl) {
+      file.clearImage(productBeforeUpdated.imageUrl)
+    } else {
+      file.clearImage(imageUrl)
+    }
   }
 };
